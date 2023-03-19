d580a35fbe0b76385ed52e7d9875740d68b185f3
mrtannguyen1 <128000077+mrtannguyen1@users.noreply.github.com>
2023-03-19 19:22:38 -0400

    Update TestFanApp.java

--- Module_1/FanApp/TestFanApp.java
+++ Module_1/FanApp/TestFanApp.java
@@ -7,17 +7,16 @@ two instances of the Fan class, one using the default constructor and the other 
 argument constructor. For the second argument constructor, use the classes constants to
 set the speed. Display the objects by invoking the classes toString() method. 
 */
-import java.util.*;
 
 // Define a public class named TestFanApp
 public class TestFanApp {
     public static void main(String[] args) {
         // Create a default fan using the no-argument constructor
-        Fan option1 = new Fan();
+        Fan fan1 = new Fan();
         // Create a fan with specific properties using the argument constructor
-        Fan option2 = new Fan(Fan.MEDIUM, true, 8, "blue");        
+        Fan fan2 = new Fan(Fan.MEDIUM, true, 8, "blue");        
         // Display the properties of fan1 and fan2 using the toString method
-        System.out.println(option1.toString());
-        System.out.println(option2.toString());
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
     }// end main
 }// end TestFanApp

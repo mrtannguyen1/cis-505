52f4b4b1db0a78647bb31807600fba6a7b5c806b
mrtannguyen1 <128000077+mrtannguyen1@users.noreply.github.com>
2023-03-19 11:19:22 -0400

    Update NguyenHelloWorld.java

--- Module_1/HelloWorldApp/NguyenHelloWorld.java
+++ Module_1/HelloWorldApp/NguyenHelloWorld.java
@@ -2,21 +2,21 @@
     Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
         Comprehensive Version (12th ed.). Pearson Education, Inc.
     Modified by R. Krasso 2021
-    Additional modifications by Nguyen T. Student 2023
+    Additional modifications by  T. Nguyen Student 2023
 */ 
 public class NguyenHelloWorld {
 
     /**
-     * Method with two arguments, which are used to print the user's first and last name.
+     * Method with two arguments, which are used to print Hello World from the user's first and last name.
      * @param firstName String 
      * @param lastName String 
      * @return prints the results to the console window.
      */
-    public static void myName(String firstName, String lastName) {
-        System.out.println(String.format("My name is %s %s", firstName, lastName));
-    } // end myName
+    public static void helloWorld(String firstName, String lastName) {
+        System.out.println(String.format("Hello World from %s %s", firstName, lastName));
+    } // end helloWorld
 
     public static void main(String[] args) {
-        myName("Tan", "Nguyen"); // Call the myName() method with the values "Tan" and "Nguyen."
+        helloWorld("Tan", "Nguyen"); // Call the myName() method with the values "Tan" and "Nguyen."
     } // end main
-} // end CodeCommentExample01
+} // end NguyenHelloWorld

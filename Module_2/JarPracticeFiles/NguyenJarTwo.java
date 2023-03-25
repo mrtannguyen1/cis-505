a33ba69ac7bd6bca5f3394e3cd44c994489cee7c
mrtannguyen1 <tannguyen1@my365.bellevue.edu>
2023-03-25 17:44:27 -0400

    added Jar

---
+++ Module_2/JarPracticeFiles/NguyenJarTwo.java
@@ -0,0 +1,22 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by  T. Nguyen Student 2023
+*/ 
+public class NguyenHelloWorld {
+
+    /**
+     * Method with two arguments, which are used to print Hello World from the user's first and last name.
+     * @param firstName String 
+     * @param lastName String 
+     * @return prints the results to the console window.
+     */
+    public static void helloWorld(String firstName, String lastName) {
+        System.out.printf(String.format("Hello World from %s %s \n", firstName, lastName));
+    } // end helloWorld
+
+    public static void main(String[] args) {
+        helloWorld("Tan", "Nguyen"); // Call the helloWorld() method with the values "Tan" and "Nguyen."
+    } // end main
+} // end NguyenHelloWorld
